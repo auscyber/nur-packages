731e45e5c874657b75aff16adba84dd011b63e4b
Ivy Pierlot <ivyp@outlook.com.au>
2025-06-16 22:14:43 +1000

    zen

--- default.nix
+++ default.nix
@@ -7,6 +7,9 @@
 #     nix-build -A mypackage
 
 { pkgs ? import <nixpkgs> { } }:
+let
+  sources = pkgs.callPackage ./_sources/generated.nix { };
+in
 
 {
   # The `lib`, `modules`, and `overlays` names are special
@@ -14,7 +17,9 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  zen-browser = pkgs.callPackage ./pkgs/zen-browser { source = sources.zen-browser; sourceRoot = "Zen.app"; };
+  zen-browser-twilight = pkgs.callPackage ./pkgs/zen-browser  { source = sources.zen-browser-twilight; sourceRoot = "Twilight.app"; };
+
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
---
+++ pkgs/zen-browser/default.nix
@@ -0,0 +1,35 @@
+{
+  source,
+  lib,
+  stdenvNoCC,
+  undmg,
+  sourceRoot,
+  ...
+}:
+
+stdenvNoCC.mkDerivation {
+  inherit (source) pname version src;
+  inherit sourceRoot;
+
+
+  nativeBuildInputs = [ undmg ];
+
+  preferLocalBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/Applications/${sourceRoot}
+    cp -r . $out/Applications/${sourceRoot}
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Privacy-focused browser that blocks trackers; ads; and other unwanted content while offering the best browsing experience!";
+    homepage = "https://github.com/zen-browser/desktop";
+    license = lib.licenses.mpl20;
+    platforms = [ "aarch64-darwin" ];
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+  };
+}
