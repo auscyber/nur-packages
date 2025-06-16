cd63eb1f5bc2ad7e5c50fe5a02f39469631470d1
Ivy Pierlot <ivyp@outlook.com.au>
2025-06-16 22:46:47 +1000

    make better

--- flake.nix
+++ flake.nix
@@ -10,5 +10,18 @@
         pkgs = import nixpkgs { inherit system; };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+devShells = forAllSystems (system: {
+        default =
+          let
+            pkgs = import nixpkgs { inherit system; };
+
+          in
+          pkgs.mkShellNoCC {
+            packages = [
+              pkgs.nvfetcher
+            ];
+          };
+      });
+
     };
 }
